27e44e057c5eff096d59deecfd87e227d7ea3b18
slavoski <slavoski@users.noreply.github.com>
2016-04-17 00:30:28 -0600

    Added in a new tab and mag-lev train. Working on getting it names.

--- Enhanced_Railroad_Pack/data.lua
+++ Enhanced_Railroad_Pack/data.lua
@@ -1,4 +1,8 @@
-require("prototypes.item")
-require("prototypes.entity")
-require("prototypes.recipe")
-require("prototypes.technology")
+require "util"
+
+
+require("prototypes.item.item-groups")
+require("prototypes.item.item")
+require("prototypes.entity.entities")
+require("prototypes.recipe.recipe")
+require("prototypes.categories.rail-category")
---
+++ Enhanced_Railroad_Pack/prototypes/categories/rail-category.lua
@@ -0,0 +1,31 @@
+--Added new categories SW 2016/04/14
+
+data:extend(
+{
+  {
+    type = "rail-category",
+    name = "electric"
+  },
+  {
+    type = "rail-category",
+    name = "mag-lev"
+  },
+  {
+    type = "rail-category",
+    name = "regular-raised"
+  },
+  {
+    type = "rail-category",
+    name = "electric-raised"
+  },
+  {
+    type = "rail-category",
+    name = "mag-lev-raised"
+  },
+  {
+    type = "rail-category",
+    name = "tunnel"
+  }
+  
+}
+)
