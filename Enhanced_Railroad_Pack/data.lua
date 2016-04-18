5c5a24473777f4db821bb3c091460bf2b4e31807
binford35 <binford35@gmail.com>
2016-04-17 19:32:32 -0500

    edited data.lua

--- Enhanced_Railroad_Pack/data.lua
+++ Enhanced_Railroad_Pack/data.lua
@@ -6,4 +6,4 @@ require("prototypes.item.item")
 require("prototypes.entity.entities")
 require("prototypes.recipe.recipe")
 require("prototypes.categories.rail-category")
-require("prototypes.technology.technology"
+require("prototypes.technology.technology")
